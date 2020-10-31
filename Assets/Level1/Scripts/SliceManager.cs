ad09d76d0e443596f3a66a9c97aa9002c890eeea
Ariestellar <ahdrei47@gmail.com>
2020-10-31 23:06:13 +0300

    Initial commit

---
+++ Assets/Level1/Scripts/SliceManager.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using EzySlice;
+using System;
+
+public class SliceManager : MonoBehaviour
+{
+    [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private GameObject _sliceLine;
+    [SerializeField] private GameObject objectToShatter;
+    [SerializeField] private Material crossSectionMaterial;
+    [SerializeField] private List<GameObject> _sliceLines;
+    [SerializeField] private List<GameObject> _pieces;
+    
+    private GameObject _currentSliceLine;
+    private Camera _camera;    
+    private bool _isMouseButtonPressed;
+    private Vector3 _startPointSwipe;
+    private Vector3 _currentPointSwipe;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {           
+            RaycastHit hit;
+            Ray rayAttack = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(rayAttack, out hit) && EventSystem.current.IsPointerOverGameObject() == false)
+            {
+                if (hit.transform.gameObject.tag == "Table")
+                {
+                    if (_isMouseButtonPressed == false)
+                    {
+                        _startPointSwipe = hit.point;
+
+                        _lineRenderer.SetPosition(0, _startPointSwipe);
+                        _currentSliceLine = Instantiate(_sliceLine);
+                        _isMouseButtonPressed = true;                        
+                    }
+                    else
+                    {
+                        _currentPointSwipe = hit.point;
+                        _lineRenderer.SetPosition(1, _currentPointSwipe);
+
+                        _currentSliceLine.transform.position = GetCenterCutPosition(_startPointSwipe, _currentPointSwipe); 
+                        Vector3 directionLook = _currentPointSwipe - _currentSliceLine.transform.position;
+                        _currentSliceLine.transform.LookAt(_currentPointSwipe, directionLook.normalized);
+                        _currentSliceLine.transform.localScale = new Vector3(_currentSliceLine.transform.localScale.x, _currentSliceLine.transform.localScale.y, Vector3.Distance(_startPointSwipe, _currentPointSwipe));
+                        _currentSliceLine.transform.position = new Vector3(_currentSliceLine.transform.position.x, 0, _currentSliceLine.transform.position.z);                        
+                    }                    
+                }
+            }           
+        }
+        else
+        {
+            if (_currentSliceLine != null)
+            {
+                _sliceLines.Add(_currentSliceLine);
+                _currentSliceLine = null;
+            }            
+            _isMouseButtonPressed = false;            
+        }
+    }
+
+    /*
+     * Нарезка
+     * -висит на кнопке "Нарезать"
+     */
+    public void Slice()
+    {
+        //Первая линия режет целый начальный объект:
+        _pieces = GetSlicedPieces(objectToShatter, GetСutLine(_sliceLines[0].transform), crossSectionMaterial);
+        objectToShatter.SetActive(false);//Цельный кусок уже ненужен, скрываем его
+
+        //временные куски для того что бы в процессе обработки они не добавлялись в актуальные и не делали цикл бесконечным\
+        //в конце они объединяються и становяться актуальными             
+        List<GameObject> tempPieces = new List<GameObject>();
+
+        //Режем куски от полученных кусков линиями по порядку:   
+        for (int i = 1; i < _sliceLines.Count; i++)
+        {
+            foreach (var item in _pieces)
+            {
+                tempPieces.AddRange(GetSlicedPieces(item, GetСutLine(_sliceLines[i].transform), crossSectionMaterial));                
+            }            
+            _pieces.Clear();
+            _pieces.AddRange(tempPieces);
+        }
+    }
+
+    /*
+     * Получить позицию линии разреза, посередине, между началом и концом свайпа (Vector3)*
+     * принимает параметры:
+     * -начало линии разреза (Vector3)
+     * -конец линии разреза (Vector3)
+     */
+    private Vector3 GetCenterCutPosition(Vector3 startLine, Vector3 endLine)
+    {
+        return new Vector3((startLine.x + endLine.x) / 2, (startLine.y + endLine.y) / 2, (startLine.z + endLine.z) / 2);
+    }
+
+    /*
+     * Получить нарезанные кусочки (List<GameObject>)
+     * принимает параметры:
+     * -объект который нужно нарезать (GameObject)
+     * -линию разреза (EzySlice.Plane)
+     * -материал который будет виден после разреза (Material)
+     */
+    private List<GameObject> GetSlicedPieces(GameObject obj, EzySlice.Plane cutLine, Material crossSectionMaterial = null)
+    {
+        GameObject[] currentShatterObject = obj.SliceInstantiate(cutLine, new TextureRegion(0.0f, 0.0f, 1.0f, 1.0f), crossSectionMaterial);
+        List<GameObject> currentShatterObjectList = new List<GameObject>();
+        if (currentShatterObject != null)
+        {            
+            foreach (var item in currentShatterObject)
+            {
+                currentShatterObjectList.Add(item);
+            }
+            obj.SetActive(false);
+        }               
+        return currentShatterObjectList;
+    }
+
+    /*
+     * Получить линию разреза (EzySlice.Plane)
+     * принимает параметры:
+     * -положение разреза (Transform)
+     */
+    private EzySlice.Plane GetСutLine(Transform incisionPosition)
+    {
+        return new EzySlice.Plane(incisionPosition.position, incisionPosition.right);
+    }    
+}
---
+++ Assets/Level1/Scripts/SlicePlane.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+
+public class SlicePlane : MonoBehaviour
+{
+	public void OnDrawGizmos()
+	{
+		EzySlice.Plane cuttingPlane = new EzySlice.Plane();		
+	}
+}
