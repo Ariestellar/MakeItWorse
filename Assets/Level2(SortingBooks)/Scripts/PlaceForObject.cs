4afd98c078209fcfb5990b6576b316aef8cbc595
Ariestellar <ahdrei47@gmail.com>
2020-11-17 11:14:53 +0300

    Анимация книг

--- Assets/Level2(SortingBooks)/Scripts/PlaceForObject.cs
+++ Assets/Level2(SortingBooks)/Scripts/PlaceForObject.cs
@@ -4,21 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Компонент вешается на пустое место заготовленное для объекта
-[RequireComponent(typeof(MeshRenderer))]
 public class PlaceForObject : MonoBehaviour
 {
     [SerializeField] private ColorType _colorType;
-
+    [SerializeField] private MeshRenderer _meshRenderer;
     private bool _isCorrectColorType;    
-    private bool _isFilled;    
-    private MeshRenderer _meshRenderer;
+    private bool _isFilled;        
+    private Animator _animator;
 
     public bool IsFilled { get => _isFilled;}
     public bool IsCorrectColorType { get => _isCorrectColorType; }
 
     private void Awake()
-    {
-        _meshRenderer = GetComponent<MeshRenderer>();
+    {        
+        _animator = GetComponent<Animator>();
     }
 
     public void PutBookInPlace(Material[] materialsBook, ColorType colorType)
@@ -32,6 +31,8 @@ public class PlaceForObject : MonoBehaviour
             _isCorrectColorType = false;
         }
         _meshRenderer.enabled = true;
+        _animator.SetTrigger("ShiftBook");
+
         _meshRenderer.materials = materialsBook;
         _isFilled = true;
     }
@@ -39,6 +40,8 @@ public class PlaceForObject : MonoBehaviour
     public void EmptyPlace()
     {
         _meshRenderer.enabled = false;
+        _animator.SetTrigger("HideBook");
+        
         _isFilled = false;
     }
 }
